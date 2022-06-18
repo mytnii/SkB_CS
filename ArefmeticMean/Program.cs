626bbc47d70e58a9095bbefa6ab8f60ea7bb73da
Roman <AverkievRE@yandex.ru>
2022-06-18 12:47:47 +0300

    реализован подсчет среднего арифметического по предметам

--- ArefmeticMean/Program.cs
+++ ArefmeticMean/Program.cs
@@ -73,6 +73,7 @@ namespace ArithmeticMean
             if (age == 0)
             {
                 Console.WriteLine("Некоректный ввод возраста");
+                return;
             }
 
             Console.WriteLine("Email?");
@@ -124,6 +125,76 @@ namespace ArithmeticMean
                 Console.Write($"{i}\t");
             }
             Console.WriteLine();
+
+            double sumOfPoints = TotalPointsBySubject    //
+                (                                        //
+                ref programmingScores,                   // Сумма баллов
+                ref mathScores,                          //
+                ref physicsScores                        //
+                ) ;                                      // 
+
+            double averageScore = sumOfPoints / NumberOfRatings   //
+                (                                                 //
+                ref programmingScores,                            //  Средний балл
+                ref mathScores,                                   //
+                ref physicsScores                                 //
+                );                                                //
+
+            Console.WriteLine($"Сумма баллов по всем предметам: {sumOfPoints}");
+            Console.WriteLine($"Среднее арифметическое балов по предметам: {averageScore}");
+        }
+        /// <summary>
+        /// Сумма баллов по всем предметам
+        /// </summary>
+        /// <param name="programmingScores">Баллы по программированию</param>
+        /// <param name="mathScores">Баллы по математике</param>
+        /// <param name="physicsScores">Баллы по физике</param>
+        /// <returns>Сумма балов по предметам</returns>
+        public static double TotalPointsBySubject
+            (
+            ref double[] programmingScores, 
+            ref double[] mathScores,
+            ref double[] physicsScores
+            )
+        {
+            double sumOfPoints = 0;
+
+            foreach(int i in programmingScores)
+            {
+                sumOfPoints += i;
+            }
+
+            foreach(int i in mathScores)
+            {
+                sumOfPoints += i;
+            }
+
+            foreach(double i in physicsScores)
+            {
+                sumOfPoints += i;
+            }
+
+            return sumOfPoints;
+        }
+
+        /// <summary>
+        /// Подщет количества баллов по предметам
+        /// </summary>
+        /// <param name="programmingScores">Баллы по программированию</param>
+        /// <param name="mathScores">Баллы по математике</param>
+        /// <param name="physicsScores">Баллы по физике</param>
+        /// <returns>Количество баллов по предметам</returns>
+        public static int NumberOfRatings
+            (
+            ref double[] programmingScores,
+            ref double[] mathScores,
+            ref double[] physicsScores
+            )
+        {
+            return 
+                programmingScores.Length + 
+                mathScores.Length + 
+                physicsScores.Length;
         }
     }
 }
