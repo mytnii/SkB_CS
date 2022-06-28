cec07ea5b0c332db3ef6a96c6f20c2e41b17be56
Roman <AverkievRE@yandex.ru>
2022-06-28 13:55:54 +0300

    Добавлен класс Employee

---
+++ PracticalWork_7/EmployeeHandbook/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeHandbook
+{
+    internal class Employee
+    {
+        public int id;
+        public DateTime date;
+        public string lastName;
+        public string firstName;
+        public string patronumic;
+        public int age;
+        public int growth;
+        public DateTime brithDate;
+        public string brithPlace;
+
+
+    }
+}
