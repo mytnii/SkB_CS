a44ce5bb5fedaecacd8bb932ac05b1224fd7e9fd
Roman <AverkievRE@yandex.ru>
2022-07-11 10:48:18 +0300

    Добавлен класс работа с файлом

---
+++ PracticalWork_7/EmployeeHandbook/FileHandling.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeHandbook
+{
+    /// <summary>
+    /// Класс работа с файлом
+    /// </summary>
+    internal class FileHandling
+    {
+    }
+}
