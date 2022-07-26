e3908838ceb8d07eb62ffadc84ea7315f1527298
Roman <AverkievRE@yandex.ru>
2022-07-26 19:00:53 +0300

    Инициализация переменных

---
+++ PracticalWork_8/PhoneBook/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class Owner
+    {
+        private string _lastName;
+        private string _firstName;
+        private string _patronumic;
+    }
+}
