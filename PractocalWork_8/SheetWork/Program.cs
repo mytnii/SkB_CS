16e9cf5013b6433d94c399d07ce3743a1679ed98
Roman <AverkievRE@yandex.ru>
2022-07-26 12:39:18 +0300

    добавлен коструктор по умолчанию и метод заполняющий лист

---
+++ PractocalWork_8/SheetWork/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SheetWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkSheet numbers = new WorkSheet();
+            numbers.SheetFilling();
+        }
+    }
+}
---
+++ PractocalWork_8/SheetWork/WorkSheet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheetWork
+{
+    /// <summary>
+    /// Класс работа с листом
+    /// </summary>
+    internal class WorkSheet
+    {
+        private List<int> _numbers; // Лист целых чисел
+
+        public WorkSheet()
+        {
+            _numbers = new List<int>();
+        }
+
+        /// <summary>
+        /// Заполнение листа
+        /// </summary>
+        public void SheetFilling()
+        {
+            Random random = new Random();
+
+            for(int i = 0; i < 100; i++)
+            {
+                this._numbers.Add(random.Next(101));
+            }
+        }
+    }
+}
