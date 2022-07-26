069d94cfb1accd0b42b2557f20e601121c3f4bd8
Roman <AverkievRE@yandex.ru>
2022-07-26 12:42:11 +0300

    Добавлен класс ConsoleOperation

---
+++ PractocalWork_8/SheetWork/ConsoleOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheetWork
+{
+    /// <summary>
+    /// работа с консолью
+    /// </summary>
+    internal class ConsoleOperation
+    {
+    }
+}
