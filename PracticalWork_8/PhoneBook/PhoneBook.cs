870f82755e036d0091d6c7d422c9a0e2b0e1b50e
Roman <AverkievRE@yandex.ru>
2022-07-28 11:04:37 +0300

    добавлен конструктор по умолчанию

---
+++ PracticalWork_8/PhoneBook/PhoneBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    /// <summary>
+    /// Телефонная книга
+    /// </summary>
+    internal class PhoneBook
+    {
+       private List<Dictionary<string, Owner>> _phoneBook;
+
+        public PhoneBook()
+        {
+            this._phoneBook = new List<Dictionary<string,Owner>>();
+        }
+    }
+}
