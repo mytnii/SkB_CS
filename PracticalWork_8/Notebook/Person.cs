6a82020d9150cba828991fa97f812d5e5500ca5a
Roman <AverkievRE@yandex.ru>
2022-08-07 12:05:21 +0300

    Добавлены поля

---
+++ PracticalWork_8/Notebook/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+{
+    internal class Person
+    {
+        #region Поля
+
+        private string _lastName;
+        private string _firstName;
+        private string _patronumic;
+
+        #endregion
+    }
+}
