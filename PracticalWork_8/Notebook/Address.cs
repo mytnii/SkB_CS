bbd89b5e318a13112186e09a1edf384e3337b2c6
Roman <AverkievRE@yandex.ru>
2022-08-07 14:12:36 +0300

    Добавлены свойства

--- PracticalWork_8/Notebook/Address.cs
+++ PracticalWork_8/Notebook/Address.cs
@@ -12,9 +12,42 @@ namespace Notebook
     internal class Address
     {
         #region Поля
+
         private string _street;      // Название улици
         private int _houseNumber;    // Номер дома
         private int _flatNumber;     // Номер квартиры
+
+        #endregion
+
+        #region Свойства
+
+        /// <summary>
+        /// Название улици
+        /// </summary>
+        public string Street
+        {
+            get { return _street; }
+            set { _street = value; }
+        }
+
+        /// <summary>
+        /// Номер дома
+        /// </summary>
+        public int HouseNumber
+        {
+            get { return _houseNumber; }
+            set { _houseNumber = value; }
+        }
+
+        /// <summary>
+        /// Номер квартиры
+        /// </summary>
+        public int FlatNumber
+        {
+            get { return _flatNumber; }
+            set { _flatNumber = value; }
+        }
+
         #endregion
     }
 }
