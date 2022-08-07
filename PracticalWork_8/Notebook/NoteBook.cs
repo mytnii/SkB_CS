dbf6f4e5ea18923ada08ad702842d378c66bab35
Roman <AverkievRE@yandex.ru>
2022-08-07 17:03:18 +0300

    Добавлен конструктор по умолчанию

--- PracticalWork_8/Notebook/NoteBook.cs
+++ PracticalWork_8/Notebook/NoteBook.cs
@@ -16,7 +16,20 @@ namespace Notebook
         private Person _person;           // Человек
         private List<Address> _addresses; // Список адресов
         private List<Phones> _phones;     // Список телефонных номеров
-        
+
+        #endregion
+
+        #region Конструкторы
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public NoteBook()
+        {
+            _person = new Person();
+            _addresses = new List<Address>();
+            _phones = new List<Phones>();
+        }
         #endregion
     }
 }
