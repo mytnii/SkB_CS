47b46d5754f544d75ca50220be8bb05d54e4c589
Roman <AverkievRE@yandex.ru>
2022-08-07 14:08:38 +0300

    Добавлены поля

--- PracticalWork_8/Notebook/Address.cs
+++ PracticalWork_8/Notebook/Address.cs
@@ -11,5 +11,10 @@ namespace Notebook
     /// </summary>
     internal class Address
     {
+        #region Поля
+        private string _street;      // Название улици
+        private int _houseNumber;    // Номер дома
+        private int _flatNumber;     // Номер квартиры
+        #endregion
     }
 }
