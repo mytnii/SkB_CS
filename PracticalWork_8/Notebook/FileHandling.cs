3f08cd87192d853c20c53189f77f98d1e589f519
Roman <AverkievRE@yandex.ru>
2022-08-10 15:07:17 +0300

    добавлен метод  serializeNotebook

---
+++ PracticalWork_8/Notebook/FileHandling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Notebook
+{
+    internal class FileHandling
+    {
+
+        /// <summary>
+        /// Сериализация записной книжки
+        /// </summary>
+        /// <param name="ConcretNoteBook">Экземпляр для сериализации</param>
+        /// <param name="file">Имя файла</param>
+        public static void SerializeNotebook(ref NoteBook ConcretNoteBook, ref string file)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(NoteBook));
+
+            FileStream fStream = new FileStream(file, FileMode.Append);
+
+            xmlSerializer.Serialize(fStream, ConcretNoteBook);
+
+            fStream.Close();
+        }
+    }
+}
